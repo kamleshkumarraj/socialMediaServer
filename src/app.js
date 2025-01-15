66abd09181975fdc751ed4bee30b6be18f9fbd79
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-01-15 17:08:28 +0530

    we add the all global prebuilt required middleware in app file

--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
 
 export const app = express();
+
+app.use(cors({
+    origin : ['http://localhost:5173' , 'http://localhost:5174' , 'http://localhost:3000'],
+    methods : ['GET', 'POST', 'PUT', 'DELETE' , 'PATCH'],
+    credentials : true
+}))
+
+app.use(cookieParser())
+
+app.use(express.urlencoded({
+    extended : true
+}))
 
 app.use(express.json({
     limit : '20kb'
 }))
+
+
+
 
 app.use((err , req , res , next) => {
     err.message = err.message || "Internal Server Error",
