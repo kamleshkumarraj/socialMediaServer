56336b2a7e4a28f641e5228a7374885ffb7e598e
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-01-18 17:07:50 +0530

    we create model for messages

---
+++ src/models/messages.models.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose"
+
+const messageSchema = new mongoose.Schema({
+    sender : {
+        type : mongoose.Schema.ObjectId,
+        ref : "user"
+    },
+    receiver : {
+        type : mongoose.Schema.ObjectId,
+        ref : "user"
+    },
+    content : {
+        type : String,
+        required : [true , "message must be required !"]
+    },
+    attachments : [{
+        url : {
+            type : String,
+            required : [true , "attachment url must be required !"]
+        },
+        publicId : {
+            type : String,
+            required : [true , "attachment publicId must be required !"]
+        }
+    }]
+})
+
+export const Messages = mongoose.model("Message" , messageSchema)
