23c02f2c671be16db0a5a0be095ba0200a76058b
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-01-19 06:49:05 +0530

    we write the controller for creating a new groupChat

--- src/controllers/users/chat.controller.js
+++ src/controllers/users/chat.controller.js
@@ -1,3 +1,19 @@
 import { asyncErrorHandler } from "../../errors/asynHandler.error.js";
+import { Chats } from "../../models/chat.models.js";
+import { sendResponse } from "../../utils/sendResponse.js";
 
-export const createGroupChat = asyncErrorHandler
+export const createGroupChat = asyncErrorHandler(async (req, res, next) => {
+    const {sendMembers = [], creator, chatName} = req.body;
+
+    const members = Set(sendMembers);
+
+    if(creator != req.user.id.toString()) return next(new ErrorHandler("You are not allowed to create group chat !", 400));
+
+    if(!members.includes(creator)) members.push(req.user.id)
+
+    if(members.length < 3) return next(new ErrorHandler("Group chat should have atleast 3 members !", 400));
+
+    await Chats.create({chatname : chatName , members , creator , groupChat : true});
+
+    sendResponse({res , status : 201 , data : null , message : 'Group chat created successfully !'})
+})
