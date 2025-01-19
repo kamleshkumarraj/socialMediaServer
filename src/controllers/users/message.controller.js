b4f01156c0223caf36a3922d4292d15a2fa6c397
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-01-19 09:16:11 +0530

    we write controller for create message nad send attachment

---
+++ src/controllers/users/message.controller.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import { asyncErrorHandler } from "../../errors/asynHandler.error.js";
+import { ErrorHandler } from "../../errors/errorHandler.errors.js";
+import { Messages } from "../../models/messages.models.js";
+import { removeFile, uploadFilesOnCloudinary } from "../../utils/cloudinary.utils";
+import { sendResponse } from "../../utils/sendResponse.js";
+
+export const createMessage = asyncErrorHandler(async (req, res, next) => {
+    const {chatId , content , attachments = [] , members = []} = req.body;
+    
+    const sendingMembers = members.filter(member => member.toString() !== req.user.id.toString()) 
+
+    await Messages.create({chatId , sender : req.user.id , receiver : sendingMembers , content , attachments})
+
+    sendResponse({res , status : 200 , data : null , message : 'Message sent successfully !'})
+})
+
+export const sendAttachment = asyncErrorHandler(async (req, res, next) => {
+    const {chatId , members = [], content} = req.body;
+
+    const attachmentsFile = req.files || [];
+
+    if(attachments.length < 1 && attachments.length > 5) return next(new ErrorHandler("You can not send more than 5 attachments less than 1 attachments !",400))
+
+    
+    const {success, results, error} = await uploadFilesOnCloudinary({files : attachmentsFile})
+    if(!success) return next(new ErrorHandler(error.message || "Error while uploading the attachments !",400))
+
+    const attachmentForDB = results.map(result => ({publicIid : result.public_id , url : result.url}))
+
+    const dbMessage = {
+        chatId : chatId,
+        sender : req.user.id,
+        receiver : members,
+        content : content,
+        attachments : attachmentForDB
+    }
+
+    const messageWithAttachment = await Messages.create(dbMessage);
+    await removeFile({files : attachmentsFile})
+
+    sendResponse({res , status : 200 , data : messageWithAttachment , message : 'Message sent successfully !'})
+})
+
+export const deleteMessage = asyncErrorHandler(async (req, res, next) => {
+    const messageId = req.params.id;
+
+    if(mongoose.isValidObjectId(message))
+})
