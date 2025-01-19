63079bc916ffbfe79415146a1c42bdd49916df66
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-01-19 11:20:27 +0530

    we write controller for share the posts

--- src/controllers/users/reactions.controller.js
+++ src/controllers/users/reactions.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import { asyncErrorHandler } from "../../errors/asynHandler.error.js";
 import { Posts } from "../../models/posts.models.js";
 import { sendResponse } from "../../utils/sendResponse.js";
@@ -28,3 +29,26 @@ export const createReactions = asyncErrorHandler(async (req, res, next) => {
 
   sendResponse({res , status : 200 , data : null , message : 'Reaction created successfully !'})
 });
+
+export const createShare = asyncErrorHandler(async (req, res, next) => {
+    const postId = req.params.id;
+    if(mongoose.isValidObjectId(postId) == false) return next(new ErrorHandler("Please send valid post id !", 400)) 
+
+    const post = await Posts.findById(postId);
+
+    if (!post) return next(new ErrorHandler("Please send valid post id !", 400));
+
+    if(post.shares.find((share) => share.creator.toString() == req.user.id.toString())) {
+        await Posts.updateOne(
+            { _id: postId, shares: { $elemMatch: { creator: req.user.id } } },
+            { $set: {$inc : { "shares.$.count" : 1}} }
+          );
+    }else{
+        await Posts.updateOne(
+            { _id: postId },
+            { $push: { shares: { creator: req.user.id, count : 1 } } }
+        )
+    }
+
+    sendResponse({res , status : 200 , data : null , message : 'Share created successfully !'})
+})
