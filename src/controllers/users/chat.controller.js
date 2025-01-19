a0dd418929ad21fc52d6db238ebdce8951532fdd
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-01-19 07:04:28 +0530

    we create controller for getting my Chats

--- src/controllers/users/chat.controller.js
+++ src/controllers/users/chat.controller.js
@@ -3,24 +3,77 @@ import { Chats } from "../../models/chat.models.js";
 import { sendResponse } from "../../utils/sendResponse.js";
 
 export const createGroupChat = asyncErrorHandler(async (req, res, next) => {
-    const {sendMembers = [], creator, chatName} = req.body;
+  const { sendMembers = [], creator, chatName } = req.body;
 
-    const members = Set(sendMembers);
+  const members = Set(sendMembers);
 
-    if(creator != req.user.id.toString()) return next(new ErrorHandler("You are not allowed to create group chat !", 400));
+  if (creator != req.user.id.toString())
+    return next(
+      new ErrorHandler("You are not allowed to create group chat !", 400)
+    );
 
-    if(!members.includes(creator)) members.push(req.user.id)
+  if (!members.includes(creator)) members.push(req.user.id);
 
-    if(members.length < 3) return next(new ErrorHandler("Group chat should have atleast 3 members !", 400));
+  if (members.length < 3)
+    return next(
+      new ErrorHandler("Group chat should have atleast 3 members !", 400)
+    );
 
-    await Chats.create({chatname : chatName , members , creator , groupChat : true});
+  await Chats.create({ chatname: chatName, members, creator, groupChat: true });
 
-    sendResponse({res , status : 201 , data : null , message : 'Group chat created successfully !'})
-})
+  sendResponse({
+    res,
+    status: 201,
+    data: null,
+    message: "Group chat created successfully !",
+  });
+});
+
+export const getMyGroupChats = asyncErrorHandler(async (req, res, next) => {
+  const myChats = await Chats.find({
+    members: { $in: [req.user.id.toString()] },
+    groupChat: true,
+  }).populate("members", "firstname lastname avatar");
+
+  const transformData = myChats.map((chat) => ({
+    _id: chat._id,
+    chatName: chat.chatname,
+    groupChat: true,
+    creator: chat.creator,
+    avatar: chat?.members?.map((member) => member?.avatar?.url),
+    member: chat?.members?.map((member) => ({
+      _id: member._id,
+      name: member.firstname + " " + member.lastname,
+    })),
+  }));
+
+  sendResponse({
+    res,
+    status: 200,
+    data: transformData,
+    message: "My chats fetched successfully !",
+  });
+});
 
 export const getMyChats = asyncErrorHandler(async (req, res, next) => {
-    const myChats = await Chats.find({members : {$in : [req.user.id.toString()]}});
+  const myChats = await Chats.find({
+    members: { $in: [req.user.id.toString()] },
+    groupChat: false,
+  }).populate("members", "firstname lastname avatar");
+  const getSecondMember = (members = []) =>
+    members.filter((member) => member._id.toString() != req.user.id.toString());
 
-    sendResponse({res , status : 200 , data : myChats , message : 'My chats fetched successfully !'})
-})
+  const transformData = myChats.map((chat) => {
+    const [secondMember] = getSecondMember(chat.members);
+    return {
+      _id: chat._id,
+      chatName: secondMember?.firstname + " " + secondMember?.lastname,
+      groupChat: false,
+      creator: chat.creator,
+      avatar: secondMember?.avatar?.url,
+      member : chat.members
+    };
+  });
 
+  sendResponse({res , data : transformData , status : 200 , message : 'My chats fetched successfully !'})
+});
