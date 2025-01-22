1094c85b9e796c71a5d8fb17958d521edffe3037
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-01-22 17:19:45 +0530

    we changes the comment model

--- src/models/comment.models.js
+++ src/models/comment.models.js
@@ -1,39 +1,87 @@
 import mongoose from "mongoose";
 
-
-
-const commentSchema = new mongoose.Schema({
-    creator : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "user"
+const commentSchema = new mongoose.Schema(
+  {
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
     },
-    comment : [{
-        content : {
-            type : String
+    comment: [
+      {
+        content: {
+          type: String,
         },
-        reply : [{
-            creator : {
-                type : mongoose.Schema.Types.ObjectId,
-                ref : "user"
+        reply: [
+          {
+            creator: {
+              _id: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "user",
+              },
+              username: {
+                type: String,
+              },
+              avatar: {
+                type: String,
+              },
             },
-            replyComment : []
-        }],
-        like : [{
-            creator : {
-                type : mongoose.Schema.Types.ObjectId,
-                ref : "user"
+            replyComment: [
+              {
+                replyContent: "",
+                like: [
+                  {
+                    creator: {
+                      _id: {
+                        type: mongoose.Schema.Types.ObjectId,
+                        ref: "user",
+                      },
+                      username: {
+                        type: String,
+                      },
+                      avatar: {
+                        type: String,
+                      },
+                    },
+                    likeType: {
+                      type: String,
+                      enum: ["like", "dislike", "none"],
+                      default: "none",
+                    },
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+        like: [
+          {
+            creator: {
+              _id: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "user",
+              },
+              username: {
+                type: String,
+              },
+              avatar: {
+                type: String,
+              },
             },
-            likeType : {
-                type : String,
-                enum : ["like" , "dislike" , "none"],
-                default : "none"
-            }
-        }]
-    }],
-    postId : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "Posts"
-    }
-}, {timestamps : true})
+            likeType: {
+              type: String,
+              enum: ["like", "dislike", "none"],
+              default: "none",
+            },
+          },
+        ],
+      },
+    ],
+    postId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Posts",
+    },
+  },
+  { timestamps: true }
+);
 
-export const Comments = mongoose.model('Comment' , commentSchema) 
+export const Comments = mongoose.model("Comment", commentSchema);
